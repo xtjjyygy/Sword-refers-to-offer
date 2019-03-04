752ac0af583202a6eef3b8e5aaa2fca43b7335ec
xtjjyygy <38133593+xtjjyygy@users.noreply.github.com>
2019-03-04 20:48:37 +0800

    Create 从尾到头打印链表.py

---
+++ 从尾到头打印链表.py
@@ -0,0 +1,23 @@
+'''
+输入一个链表，按链表值从尾到头的顺序返回一个ArrayList。
+'''
+
+# -*- coding:utf-8 -*-
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    # 返回从尾部到头部的列表值序列，例如[1,2,3]
+    def printListFromTailToHead(self, listNode):
+        # write code here
+        if not listNode:
+            return []
+        
+        result =[]
+        
+        while listNode:
+            result.insert(0, listNode.val)
+            listNode = listNode.next
+        return result        
