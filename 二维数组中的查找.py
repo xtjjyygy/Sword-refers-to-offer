e189505ae6df7257d84703b8d175fb6a8dea8cf6
xtjjyygy <38133593+xtjjyygy@users.noreply.github.com>
2019-03-04 19:29:54 +0800

    Update 二维数组中的查找.py

--- 二维数组中的查找.py
+++ 二维数组中的查找.py
@@ -11,3 +11,26 @@ class Solution:
             if target in array[i]:
                 return True
         return False
+    
+    #附：教材上的解法：
+    # -*- coding:utf-8 -*-
+class Solution:
+    # array 二维列表
+    def Find(self, target, array):
+        # write code here
+        if array == []:
+            return False
+        num_row = len(array)
+        num_col = len(array[0])
+        
+        i = num_col - 1
+        j = 0
+        
+        while i >= 0 and j < num_row:
+            if array[j][i] > target:
+                i -= 1
+            elif array[j][i] < target:
+                j += 1
+            else:
+                return True
+    
