a40196c987aec1579c1542579c9962583a7179f4
xtjjyygy <38133593+xtjjyygy@users.noreply.github.com>
2019-03-10 17:58:58 +0800

    Update 二叉树中和为某一值的路径.py

--- 二叉树中和为某一值的路径.py
+++ 二叉树中和为某一值的路径.py
@@ -1,4 +1,9 @@
 # -*- coding:utf-8 -*-
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
 class Solution:
     # 返回二维列表，内部每个列表表示找到的路径
     def FindPath(self, root, expectNumber):
@@ -7,18 +12,18 @@ class Solution:
             return []
         ret = []
         path = []
-        self.Find(root, expectNumber, ret, path)
+        self.Find(root,expectNumber,ret,path)
         return ret
-     
-    def Find(self, root, target, ret, path):
+    
+    def Find(self,root,expectNumber,ret,path):
         if not root:
-            return
+            return []
         path.append(root.val)
-        isLeaf = (root.left is None and root.right is None)
-        if isLeaf and target == root.val:
-            ret.append(path[:])  # 这里这一步要千万注意啊， 
+        isleaf = (root.right == None and root.left == None)
+        if isleaf and expectNumber == root.val:
+            ret.append(path[:])
         if root.left:
-            self.Find(root.left, target - root.val, ret, path)
+            self.Find(root.left,expectNumber-root.val,ret,path)
         if root.right:
-            self.Find(root.right, target - root.val, ret, path)
+            self.Find(root.right,expectNumber-root.val,ret,path)
         path.pop()
