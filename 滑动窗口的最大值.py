19dcf4e309bbf3df28b2e82fa390fe55de3a17a5
xtjjyygy <38133593+xtjjyygy@users.noreply.github.com>
2019-03-21 16:05:34 +0800

    Update 滑动窗口的最大值.py

--- 滑动窗口的最大值.py
+++ 滑动窗口的最大值.py
@@ -12,4 +12,33 @@ class Solution:
         MaxNum = []
         for i in range(len(num)-size+1):
             MaxNum.append(max(num[i:i+size]))
-        return(MaxNum)        
+        return(MaxNum)       
+    
+
+    
+# -*- coding:utf-8 -*-
+class Solution:
+    def maxInWindows(self, num, size):
+        # write code here
+        if not num or size <= 0:
+            return []
+        deque = []
+        if len(num) >= size:
+            index = []
+            for i in range(size):
+                while len(index) > 0 and num[i] > num[index[-1]]:
+                    index.pop()
+                index.append(i)
+
+            for i in range(size, len(num)):
+                deque.append(num[index[0]])
+                while len(index) > 0 and num[i] >= num[index[-1]]:
+                    index.pop()
+                if len(index) > 0 and index[0] <= i - size:#判断index[0]的位置是否到达出栈的条件
+                    index.pop(0)
+                index.append(i)
+
+            deque.append(num[index[0]])
+        return deque
+sol = Solution()
+print(sol.maxInWindows(num=[4,2,3,2,6,2,5,1],size=3))    
