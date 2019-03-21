2b6fb31b24c8181f1a46c42a00e01ba8c85289fb
xtjjyygy <38133593+xtjjyygy@users.noreply.github.com>
2019-03-21 15:31:12 +0800

    Create 滑动窗口的最大值.py

---
+++ 滑动窗口的最大值.py
@@ -0,0 +1,15 @@
+'''
+题目描述
+给定一个数组和滑动窗口的大小，找出所有滑动窗口里数值的最大值。例如，如果输入数组{2,3,4,2,6,2,5,1}及滑动窗口的大小3，那么一共存在6个滑动窗口，他们的最大值分别为{4,4,6,6,6,5}； 针对数组{2,3,4,2,6,2,5,1}的滑动窗口有以下6个： {[2,3,4],2,6,2,5,1}， {2,[3,4,2],6,2,5,1}， {2,3,[4,2,6],2,5,1}， {2,3,4,[2,6,2],5,1}， {2,3,4,2,[6,2,5],1}， {2,3,4,2,6,[2,5,1]}。
+'''
+
+# -*- coding:utf-8 -*-
+class Solution:
+    def maxInWindows(self, num, size):
+        # write code here
+        if not num or not size:
+            return []
+        MaxNum = []
+        for i in range(len(num)-size+1):
+            MaxNum.append(max(num[i:i+size]))
+        return(MaxNum)        
