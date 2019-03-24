360273b0eb64237500b815fcf93fdd7ac782a4e1
xtjjyygy <38133593+xtjjyygy@users.noreply.github.com>
2019-03-24 10:27:50 +0800

    Update 从尾到头打印链表.py

--- 从尾到头打印链表.py
+++ 从尾到头打印链表.py
@@ -40,3 +40,24 @@ class Solution:
             listNode = listNode.next
         arraylist.reverse()
         return arraylist      
+#用栈  
+ # -*- coding:utf-8 -*-
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    # 返回从尾部到头部的列表值序列，例如[1,2,3]
+    def printListFromTailToHead(self, listNode):
+        # write code here
+        if not listNode:
+            return []
+        arraylist = []
+        newlist = []
+        while listNode:
+            arraylist.append(listNode.val)
+            listNode = listNode.next
+        while arraylist:
+            newlist.append(arraylist.pop())
+        return newlist
