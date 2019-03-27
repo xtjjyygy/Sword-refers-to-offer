ff767223acfc0517cc13faea7c522a55f78c81ce
xtjjyygy <38133593+xtjjyygy@users.noreply.github.com>
2019-03-27 19:54:49 +0800

    Update 数值的整数次方.py

--- 数值的整数次方.py
+++ 数值的整数次方.py
@@ -20,3 +20,29 @@ class Solution:
         for i in range(1,exponent+1):
             result*=base
         return result
+
+#用位运算来改进PowerWithUnsignedExponent函数
+# -*- coding:utf-8 -*-
+class Solution:
+    def Power(self,base,exponent):
+        g_invalidinput = False
+        if base == 0.0 and exponent < 0:
+            g_invalidinput = True
+            return 0.0
+        absExponent = abs(exponent)
+        result = self.PowerWithUnsignedExponent(base,absExponent)
+        if exponent < 0:
+            result = 1.0/result
+        return result
+    def PowerWithUnsignedExponent(self,base,exponent):
+        if exponent == 0:
+            return 1
+        if exponent == 1:
+            return base
+        result = self.PowerWithUnsignedExponent(base,exponent>>1)
+        result *= result
+        if exponent & 1 == 1:
+            result*=base
+        return result
+sol = Solution()
+print(sol.Power(base=2.0,exponent=3))
