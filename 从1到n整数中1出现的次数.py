ac212a153b8036adfe682ed3ff423ce91f51afcd
xtjjyygy <38133593+xtjjyygy@users.noreply.github.com>
2019-05-13 15:33:32 +0800

    Update 从1到n整数中1出现的次数.py

--- 从1到n整数中1出现的次数.py
+++ 从1到n整数中1出现的次数.py
@@ -23,3 +23,33 @@ class Solution:
                 count+=base;
             base*=10
         return count
+
+#剑指offer解法
+# -*- coding:utf-8 -*-
+class Solution:
+    def NumberOf1Between1AndN_Solution(self, n):
+        # write code here
+        if n <= 0:
+            return 0
+
+        return self.NumberOf1(str(n))
+    def NumberOf1(self,strN):
+        if not strN:
+            return 0
+
+        first = int(strN[0])
+        length = len(strN)
+        if length == 1 and first == 0:
+            return 0
+        if length == 1 and first>0:
+            return 1
+        numFristDigit=0
+        if first>1:
+            numFristDigit=10**(length-1)
+        elif first == 1:
+            numFristDigit = int(strN[1:])+1
+
+        numOtherDigits = first*(length-1)*10**(length-2)
+        numRecursive = self.NumberOf1(strN[1:])
+
+        return numFristDigit+numOtherDigits+numRecursive
