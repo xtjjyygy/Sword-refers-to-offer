78920a748cdd3d9782cfec2dc9ad9aa1898ebc2e
xtjjyygy <38133593+xtjjyygy@users.noreply.github.com>
2019-08-14 10:20:53 +0800

    Update LinkListQuickSort.py

--- LinkListQuickSort.py
+++ LinkListQuickSort.py
@@ -1,3 +1,42 @@
+#方法一：
+class Solution(object):
+    def sortList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        def partition(start, end):
+            node = start.next.next
+            pivotPrev = start.next
+            pivotPrev.next = end
+            pivotPost = pivotPrev
+            while node != end:
+                temp = node.next
+                if node.val > pivotPrev.val:
+                    node.next = pivotPost.next
+                    pivotPost.next = node
+                elif node.val < pivotPrev.val:
+                    node.next = start.next
+                    start.next = node
+                else:
+                    node.next = pivotPost.next
+                    pivotPost.next = node
+                    pivotPost = pivotPost.next
+                node = temp
+            return [pivotPrev, pivotPost]
+
+        def quicksort(start, end):
+            if start.next != end:
+                prev, post = partition(start, end)
+                quicksort(start, prev)
+                quicksort(post, end)
+
+        newHead = ListNode(0)
+        newHead.next = head
+        quicksort(newHead, None)
+        return newHead.next
+
+#方法二
 class Node():
     def __init__(self, item=None):
         self.item = item
