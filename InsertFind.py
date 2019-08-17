c4a4de21dbc703dbb0008e7f68a0f060a5aca370
xtjjyygy <38133593+xtjjyygy@users.noreply.github.com>
2019-08-17 16:01:00 +0800

    Update InsertFind.py

--- InsertFind.py
+++ InsertFind.py
@@ -1,14 +1,17 @@
 class Solution:
     def find(self,nums,target,start,end):
+        if start>end:
+            return False
         mid = start+(target-nums[start])//(nums[end]-nums[start])*(end-start)
         if nums[mid]==target:
             return mid
         if nums[mid]>target:
-            return self.find(nums,target,start,mid-1)
+            end = mid-1
         if nums[mid]<target:
-            return self.find(nums,target,mid+1,end)
+            start = mid+1
+        return self.find(nums,target,start,end)
 
 if __name__ == "__main__":
     nums = [5,6,7,8,9,10,11,12]
-    target=6
+    target=10
     print(Solution().find(nums,target,0,len(nums)-1))
